b07540c698d8cee6937393631dc8810501c982d1
johnsonlee <g.johnsonlee@gmail.com>
2020-04-20 02:43:18 +0800

    Rename benchmark test

--- asm-vs-javassist/src/jmh/kotlin/io/johnsonlee/booster/benchmark/JavassistVsAsmBenchmark.kt
+++ asm-vs-javassist/src/jmh/kotlin/io/johnsonlee/booster/benchmark/JavassistVsAsmBenchmark.kt
@@ -1,10 +1,9 @@
 package io.johnsonlee.booster.benchmark
 
 import com.didiglobal.booster.build.AndroidSdk
-import com.didiglobal.booster.transform.AbstractTransformContext
 import com.didiglobal.booster.transform.asm.AsmTransformer
 import com.didiglobal.booster.transform.javassist.JavassistTransformer
-import com.didiglobal.booster.transform.util.transform
+import com.didiglobal.booster.transform.util.TransformHelper
 import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.BenchmarkMode
 import org.openjdk.jmh.annotations.Fork
@@ -16,13 +15,12 @@ import org.openjdk.jmh.annotations.State
 import org.openjdk.jmh.annotations.TearDown
 import java.io.File
 import java.util.concurrent.TimeUnit
-import java.util.jar.JarFile
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Fork(value = 2, jvmArgs = ["-Xms2G", "-Xmx2G"])
 @State(Scope.Benchmark)
-open class ClassLoadBenchmark {
+open class JavassistVsAsmBenchmark {
 
     private lateinit var file: File
 
@@ -38,34 +36,14 @@ open class ClassLoadBenchmark {
 
     @Benchmark
     fun transformJarUsingAsm() {
-        val target = File.createTempFile("guava-", "-28.2-jre.jar")
-        val context = object : AbstractTransformContext("transform-jar-using-asm", listOf(AndroidSdk.getAndroidJar(28)), emptyList(), emptyList()) {}
-
-        AsmTransformer(AsmThreadTransformer()).let { asm ->
-            asm.onPreTransform(context)
-
-            JarFile(this.file).transform(target) { bytecode ->
-                asm.transform(context, bytecode)
-            }
-
-            // asm.onPostTransform(context)
-        }
+        TransformHelper(this.file, AndroidSdk.getAndroidJar().parentFile)
+                .transform(transformers = *arrayOf(AsmTransformer(AsmThreadTransformer())))
     }
 
     @Benchmark
     fun transformJarUsingJavassist() {
-        val target = File.createTempFile("guava-", "-28.2-jre.jar")
-        val context = object : AbstractTransformContext("transform-jar-using-javassist", listOf(AndroidSdk.getAndroidJar(28)), emptyList(), emptyList()) {}
-
-        JavassistTransformer(JavassistThreadTransformer()).let { javassist ->
-            javassist.onPreTransform(context)
-
-            JarFile(this.file).transform(target) { bytecode ->
-                javassist.transform(context, bytecode)
-            }
-
-            // javassist.onPostTransform(context)
-        }
+        TransformHelper(this.file, AndroidSdk.getAndroidJar().parentFile)
+                .transform(transformers = *arrayOf(JavassistTransformer(JavassistThreadTransformer())))
     }
 
     @TearDown
